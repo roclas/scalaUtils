fda7176b1faaea4713b4e5032200a2f442e2d9b1
carlos hernandez <carlopolo@gmail.com>
2015-11-10 16:08:31 +0000

    new example with dynatrace

--- scalaSelenium/build.sbt
+++ scalaSelenium/build.sbt
@@ -1,4 +1,7 @@
 scalaVersion := "2.11.1"
-libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
-libraryDependencies += "org.seleniumhq.selenium" % "selenium-java" % "2.35.0" % "test"
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" 
+libraryDependencies += "org.seleniumhq.selenium" % "selenium-java" % "2.35.0" 
+libraryDependencies += "easytravel" % "easitravel" % "1.0" from "file://lib/com.dynatrace.easytravel.selenium.jar"
+libraryDependencies += "junit" % "junit" % "4.11" from "file://lib/junit-4.11.jar"
+
 
--- scalaSelenium/src/test/scala/test0.scala
+++ scalaSelenium/src/test/scala/test0.scala
@@ -2,17 +2,30 @@ import org.scalatest._
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import selenium._
+import com.dynatrace.webautomation.DynaTraceSeleniumRunner
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import com.thoughtworks.selenium.Selenium
+import com.dynatrace.webautomation.SeleniumConfiguration
+import com.dynatrace.webautomation.DynaTraceSeleniumHelper
+import com.thoughtworks.selenium.DefaultSelenium
 
+
+//@RunWith(classOf[DynaTraceSeleniumRunner])
 class BlogSpec extends FlatSpec with ShouldMatchers with WebBrowser {
 
-  implicit val webDriver: WebDriver = new HtmlUnitDriver
-
-  val host = "http://localhost:8080/"
+   //implicit val webDriver: WebDriver = new HtmlUnitDriver
+   //val host = "http://localhost:8080/"
 
   "The blog app home page" should "have the correct title" in {
-    go to (host + "/")
-    println(pageTitle)
-    pageTitle should be ("Home - Liferay")
+    //go to (host + "/")
+    //println(pageTitle)
+    //pageTitle should be ("Home - Liferay")
+    val selenium:Selenium=new DefaultSelenium("localhost", 4444, "*firefox", "http://localhost:8080")
+    selenium.start()
+    DynaTraceSeleniumHelper.forSelenium(selenium).open("/")     
+    selenium.getTitle() should be ("Home - Liferay")
   }
 }
   
